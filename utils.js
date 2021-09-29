82943ae3fce187c22a9b2159705039c50130d498
samau3 <samau415@gmail.com>
2021-09-29 12:38:53 -0700

    added error messages empty query or if a non-number is passed in

--- utils.js
+++ utils.js
@@ -6,11 +6,12 @@ const { BadRequestError } = require("./expressError");
 function convertStrNums(strNums) {
   // if the conversion isn't successful, throw a BadRequestError and will
   // be handled in your route
+  if (strNums.length === 0) throw new BadRequestError(`Numbers are required`)
   let splitNums = strNums.split(',')
   // console.log(splitNums)
   let convertedNums = splitNums.map(value => {
     let number = Number(value)
-    if (isNaN(number)) throw new BadRequestError()
+    if (isNaN(number)) throw new BadRequestError(`${value} is not a number`)
     else return number
   })
 
